d90fb5738d2929840dc061b3ab2eadb8a9e1475d
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-05-31 23:10:14 -0700

    Factory Patterns

---
+++ pets/factory.go
@@ -0,0 +1,16 @@
+package pets
+
+import "go-breeders/models"
+
+// A simple factory for pet
+func NewPet(species string) *models.Pet {
+	pet := models.Pet{
+		Species:     species,
+		Breed:       "",
+		MinWeight:   0,
+		MaxWeight:   0,
+		Description: "MNo description entered yet",
+		LifeSpan:    0,
+	}
+	return &pet
+}
