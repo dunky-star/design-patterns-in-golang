9b3d0ec8a18cbe681695563d82344135ce7d45b7
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-05-31 21:55:10 -0700

    Go find a pet

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,6 +8,9 @@ import (
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /", app.ShowHome)
 	mux.HandleFunc("GET /{page}", app.ShowPage)
 
